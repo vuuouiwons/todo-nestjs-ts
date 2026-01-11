112595ec00eedfee41a2c5d24aa06fc35794325f
Vinsensius Reinard <vinconloren@gmail.com>
2026-01-11 20:26:19 +0700

    updated healthcheck swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-
+@ApiTags('System')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get('/health')
+  @ApiOperation({ summary: 'Perform a system health check' })
+  @ApiOkResponse({
+    description: 'The service is functional and responding correctly.',
+  })
   health() {
-    return this.appService.handleHealth()
+    return this.appService.handleHealth();
   }
 }
